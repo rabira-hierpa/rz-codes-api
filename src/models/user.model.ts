6902daf70678febcc6176b54e5a71905dc62f6e7
Rz <rabira.hierpa@gmail.com>
2020-11-27 16:59:54 +0300

    created user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,39 @@
+import {Model, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class User extends Model {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  hasAccount: boolean;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
